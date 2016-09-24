5988e35e95b9eeab546763553e4426f000fa5b41
tiffaniechia <tiffaniekyrachia@gmail.com>
2016-09-24 17:09:54 +0200

    inital method to for escaping velocity

--- lib/physics/rocketry.ex
+++ lib/physics/rocketry.ex
@@ -1,3 +1,23 @@
 defmodule Physics.Rocketry do
 
+  def escape_velocity(planet) when is_map(planet) do
+    planet
+      |> calculate_escape
+      |> convert_to_km
+      |> rounded_to_nearest_tenth
+  end
+
+  defp calculate_escape(%{mass: mass, radius: radius}) do
+    2 * 6.67e-11 * mass / radius
+      |> :math.sqrt
+  end
+
+  defp convert_to_km(velocity) do
+    velocity / 1000
+  end
+
+  defp rounded_to_nearest_tenth(val) do
+    Float.ceil(val, 1)
+  end
+
 end
