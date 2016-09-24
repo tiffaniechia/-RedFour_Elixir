b4ab51e0a4a820ebfbb73a58de7547155ae5b4e4
tiffaniechia <tiffaniekyrachia@gmail.com>
2016-09-24 17:23:51 +0200

    moved convert methods into separate module

--- lib/physics/rocketry.ex
+++ lib/physics/rocketry.ex
@@ -1,23 +1,24 @@
-defmodule Physics.Rocketry do
-
-  def escape_velocity(planet) when is_map(planet) do
-    planet
-      |> calculate_escape
-      |> convert_to_km
-      |> rounded_to_nearest_tenth
-  end
-
-  defp calculate_escape(%{mass: mass, radius: radius}) do
+defmodule Converter do
+  def calculate_escape(%{mass: mass, radius: radius}) do
     2 * 6.67e-11 * mass / radius
       |> :math.sqrt
   end
 
-  defp convert_to_km(velocity) do
+  def convert_to_km(velocity) do
     velocity / 1000
+  end
+end
+
+
+defmodule Physics.Rocketry do
+  def escape_velocity(planet) when is_map(planet) do
+    planet
+      |> calculate_escape
+      |> Converter.convert_to_km
+      |> Converter.rounded_to_nearest_tenth
   end
 
   defp rounded_to_nearest_tenth(val) do
     Float.ceil(val, 1)
   end
-
 end
