78933a65ee9346dc267df1aedb1ddb72ffda5466
tiffaniechia <tiffaniekyrachia@gmail.com>
2016-10-05 16:55:53 +0100

    pulls new planets info into module attributes

--- lib/physics/rocketry.ex
+++ lib/physics/rocketry.ex
@@ -1,35 +1,22 @@
-defmodule Converter do
-  def rounded_to_nearest_tenth(val), do: Float.round(val, 1)
+defmodule Physics.Rocketry do
+  import Converter
 
-  def to_km(velocity), do: velocity / 1000
+  @moon %{mass: 7.35e22, radius: 1.738e6}
+  @mars %{mass: 6.39e23, radius: 3.4e6}
+  @earth %{mass: 5.972e24, radius: 6.371e6}
 
-  def to_meters(velocity), do: velocity * 1000
-
-  def to_light_seconds(arg), do: to_light_seconds(arg, precision: 5)
-  def to_light_seconds({unit, val}, precision: precision) do
-    case unit do
-      :meters -> from_meters(val)
-      :miles -> from_miles(val)
-      :feet -> from_feet(val)
-      :inches -> from_inches(val)
-    end |> round_to(precision)
+  def escape_velocity(:earth) do
+    @earth
+      |> escape_velocity
   end
 
-  defp from_meters(val), do: val * 3.335638620368e-9
+  def escape_velocity(:mars) do
+    @mars
+      |> escape_velocity
+  end
 
-  defp from_miles(val), do: val * 5.36819e-6
-
-  defp from_feet(val), do: val * 1.016702651488166404e-9
-
-  defp from_inches(val), do: val * 8.472522095734715723e-11
-
-  defp round_to(val, precision) when is_float(val), do: Float.round(val, precision)
-end
-
-
-defmodule Physics.Rocketry do
-  def escape_velocity(:earth) do
-    %{mass: 5.972e24, radius: 6.371e6}
+  def escape_velocity(:moon) do
+    @moon
       |> escape_velocity
   end
 
