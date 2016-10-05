aaf43f937f41d815f82b9b7d233ac5a299074e0f
tiffaniechia <tiffaniekyrachia@gmail.com>
2016-10-05 16:33:27 +0100

    refactors converter module functions into single line

--- lib/physics/rocketry.ex
+++ lib/physics/rocketry.ex
@@ -1,15 +1,9 @@
 defmodule Converter do
-  def rounded_to_nearest_tenth(val) do
-    Float.round(val, 1)
-  end
+  def rounded_to_nearest_tenth(val), do: Float.round(val, 1)
 
-  def to_km(velocity) do
-    velocity / 1000
-  end
+  def to_km(velocity), do: velocity / 1000
 
-  def to_meters(velocity) do
-    velocity * 1000
-  end
+  def to_meters(velocity), do: velocity * 1000
 
   def to_light_seconds({unit, val}, precision: precision) do
     case unit do
@@ -20,25 +14,15 @@ defmodule Converter do
     end |> round_to(precision)
   end
 
-  defp from_meters(val) do
-    val * 3.335638620368e-9
-  end
+  defp from_meters(val), do: val * 3.335638620368e-9
 
-  defp from_miles(val) do
-    val * 5.36819e-6
-  end
+  defp from_miles(val), do: val * 5.36819e-6
 
-  defp from_feet(val) do
-    val * 1.016702651488166404e-9
-  end
+  defp from_feet(val), do: val * 1.016702651488166404e-9
 
-  defp from_inches(val) do
-    val * 8.472522095734715723e-11
-  end
+  defp from_inches(val), do: val * 8.472522095734715723e-11
 
-  defp round_to(val, precision) when is_float(val) do
-    Float.round(val, precision)
-  end
+  defp round_to(val, precision) when is_float(val), do: Float.round(val, precision)
 end
 
 
