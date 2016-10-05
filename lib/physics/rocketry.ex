45a09fa09c8a26046d2ba6fd3ad1e94b99a1bce3
tiffaniechia <tiffaniekyrachia@gmail.com>
2016-10-05 17:00:26 +0100

    refactors escape velocity into single line

--- lib/physics/rocketry.ex
+++ lib/physics/rocketry.ex
@@ -4,21 +4,13 @@ defmodule Physics.Rocketry do
   @moon %{mass: 7.35e22, radius: 1.738e6}
   @mars %{mass: 6.39e23, radius: 3.4e6}
   @earth %{mass: 5.972e24, radius: 6.371e6}
+  @newtons_constant 6.67e-11
 
-  def escape_velocity(:earth) do
-    @earth
-      |> escape_velocity
-  end
+  def escape_velocity(:earth), do: @earth |> escape_velocity
 
-  def escape_velocity(:mars) do
-    @mars
-      |> escape_velocity
-  end
+  def escape_velocity(:mars), do: @mars |> escape_velocity
 
-  def escape_velocity(:moon) do
-    @moon
-      |> escape_velocity
-  end
+  def escape_velocity(:moon) do: @moon |> escape_velocity
 
   def escape_velocity(planet) when is_map(planet) do
     planet
@@ -28,7 +20,7 @@ defmodule Physics.Rocketry do
   end
 
   defp calculate_escape(%{mass: mass, radius: radius}) do
-    2 * 6.67e-11 * mass / radius
+    2 * @newtons_constant * mass / radius
       |> :math.sqrt
   end
 end
