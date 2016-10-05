56058c53c7914ee299742d196f7caf0f807a2409
tiffaniechia <tiffaniekyrachia@gmail.com>
2016-10-05 16:20:03 +0100

    adds new conversion units

--- lib/physics/rocketry.ex
+++ lib/physics/rocketry.ex
@@ -2,20 +2,33 @@ defmodule Converter do
   def rounded_to_nearest_tenth(val) do
     Float.round(val, 1)
   end
+
   def to_km(velocity) do
     velocity / 1000
   end
 
+  def to_meters(velocity) do
+    velocity * 1000
+  end
+
   def to_light_seconds({:meters, meters} = val, precision: precision) do
-    meters * 3.335638620368e-9 |> round_up
+    meters * 3.335638620368e-9 |> round_up(precision)
   end
 
   def to_light_seconds({:miles, miles} = val, precision: precision) do
-    miles * 5.36819e-6 |> round_up
+    miles * 5.36819e-6 |> round_up(precision)
   end
 
-  def round_up(val) when is_float(val) do
-    trunc(val)
+  def to_light_seconds({:feet, feet} = val, precision: precision) do
+    feet * 1.016702651488166404e-9 |> round_up(precision)
+  end
+
+  def to_light_seconds({:inches, inches} = val, precision: precision) do
+    inches * 8.472522095734715723e-11 |> round_up(precision)
+  end
+
+  def round_to(val, precision) when is_float(val) do
+    Float.round(val, precision)
   end
 end
 
