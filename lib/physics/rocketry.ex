732266ba6ffdcc8f7f39a1fa3fd8e815e82d0165
tiffaniechia <tiffaniekyrachia@gmail.com>
2016-10-05 16:28:43 +0100

    refactors to_light_seconfs into case statements

--- lib/physics/rocketry.ex
+++ lib/physics/rocketry.ex
@@ -11,23 +11,32 @@ defmodule Converter do
     velocity * 1000
   end
 
-  def to_light_seconds({:meters, meters} = val, precision: precision) do
-    meters * 3.335638620368e-9 |> round_up(precision)
+  def to_light_seconds({unit, val}, precision: precision) do
+    case unit do
+      :meters -> from_meters(val)
+      :miles -> from_miles(val)
+      :feet -> from_feet(val)
+      :inches -> from_inches(val)
+    end |> round_to(precision)
   end
 
-  def to_light_seconds({:miles, miles} = val, precision: precision) do
-    miles * 5.36819e-6 |> round_up(precision)
+  defp from_meters(val) do
+    val * 3.335638620368e-9
   end
 
-  def to_light_seconds({:feet, feet} = val, precision: precision) do
-    feet * 1.016702651488166404e-9 |> round_up(precision)
+  defp from_miles(val) do
+    val * 5.36819e-6
   end
 
-  def to_light_seconds({:inches, inches} = val, precision: precision) do
-    inches * 8.472522095734715723e-11 |> round_up(precision)
+  defp from_feet(val) do
+    val * 1.016702651488166404e-9
   end
 
-  def round_to(val, precision) when is_float(val) do
+  defp from_inches(val) do
+    val * 8.472522095734715723e-11
+  end
+
+  defp round_to(val, precision) when is_float(val) do
     Float.round(val, precision)
   end
 end
