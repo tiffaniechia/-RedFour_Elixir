94b9d9eb46d0c73d230536f5c7f6e41d053e4f4a
tiffaniechia <tiffaniekyrachia@gmail.com>
2016-10-05 17:20:01 +0100

    extracts rocketry attributes into separate modules as they did not strictly belong to rocketry

--- lib/physics/rocketry.ex
+++ lib/physics/rocketry.ex
@@ -1,17 +1,14 @@
 defmodule Physics.Rocketry do
   import Converter
   import Calculus
+  import Planets
+  import Physics.Laws
 
-  @moon %{mass: 7.35e22, radius: 1.738e6}
-  @mars %{mass: 6.39e23, radius: 3.4e6}
-  @earth %{mass: 5.972e24, radius: 6.371e6}
-  @newtons_constant 6.67e-11
+  def escape_velocity(:earth), do: earth |> escape_velocity
 
-  def escape_velocity(:earth), do: @earth |> escape_velocity
+  def escape_velocity(:mars), do: mars |> escape_velocity
 
-  def escape_velocity(:mars), do: @mars |> escape_velocity
-
-  def escape_velocity(:moon) do: @moon |> escape_velocity
+  def escape_velocity(:moon) do: moon |> escape_velocity
 
   def escape_velocity(planet) when is_map(planet) do
     planet
@@ -21,7 +18,7 @@ defmodule Physics.Rocketry do
   end
 
   defp calculate_escape(%{mass: mass, radius: radius}) do
-    2 * @newtons_constant * mass / radius
+    2 * newtons_gravitional_constant * mass / radius
       |> square_root
   end
 end
